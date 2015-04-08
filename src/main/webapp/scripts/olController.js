7fc8f5bfaa9e1fa29f0bfc4f61392a7950eddd8a
SzeN <SzeNowy@gmail.com>
2015-04-08 21:20:56 +0100

    Tabelka, kliki, its

--- src/main/webapp/scripts/olController.js
+++ src/main/webapp/scripts/olController.js
@@ -1,55 +1,29 @@
 'use strict';
 
-gisApp.controller('olController', ['$scope', function ($scope) {
-        angular.extend($scope, {
-            center: {
-                lat: 54.34766,
-                lon: 18.64542,
-                zoom: 10
-            },
-            szkoly: {
-                source: {
-                    type: 'GeoJSON',
-                    url: 'json/szkoly.json'
-                }
-            },
-            defaults: {
-                events: {
-                    map: ['singleclick', 'pointermove']
-                }
-            },
-            mouseposition: {},
-            mouseclickposition: {},
-            projection: 'EPSG:900913'
-        });
+angular.module('gisApp')
+        .controller('olController', function ($scope, SzkolyService) {
 
-        $scope.$on('openlayers.map.pointermove', function (event, coord) {
-            $scope.$apply(function () {
-                if ($scope.projection === coord.projection) {
-                    $scope.mouseposition = coord;
-                } else {
-                    var p = ol.proj.transform([coord.lon, coord.lat], coord.projection, $scope.projection);
-                    $scope.mouseposition = {
-                        lat: p[1],
-                        lon: p[0],
-                        projection: $scope.projection
+            $scope.getSzkoly = function () {
+                SzkolyService.getAll(function (result) {
+                    $scope.szkoly = result;
+                    console.log($scope.szkoly);
+                });
+            };
+            $scope.getSzkoly();
+
+
+            angular.extend($scope, {
+                center: {
+                    lat: 54.34766,
+                    lon: 18.64542,
+                    zoom: 10
+                },
+                szkoly: {},
+                defaults: {
+                    events: {
+                        map: ['singleclick', 'pointermove']
                     }
-                }
+                },
+                projection: 'EPSG:900913'
             });
         });
-
-        $scope.$on('openlayers.map.singleclick', function (event, coord) {
-            $scope.$apply(function () {
-                if ($scope.projection === coord.projection) {
-                    $scope.mouseclickposition = coord;
-                } else {
-                    var p = ol.proj.transform([coord.lon, coord.lat], coord.projection, $scope.projection);
-                    $scope.mouseclickposition = {
-                        lat: p[1],
-                        lon: p[0],
-                        projection: $scope.projection
-                    }
-                }
-            });
-        });
-    }]);
